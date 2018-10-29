32770db2d806ede6c1335e9faab3d19ff18cc860
HealyDustin2019 <42677953+HealyDustin2019@users.noreply.github.com>
2018-10-29 12:59:59 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,23 @@
 public class Main{
     public static void main(String[] args){
-        calc(8,30,3);
-        calc(8,35,3);
-        calc(8,40,3);
-        calc(9,30,3);
-        calc(9,35,3);
-        calc(9,40,3);
-        calc(10,30,3);
-        calc(10,35,3);
-        calc(10,40,3);
-        calc(11,30,3);
-        calc(11,35,3);
-        calc(11,40,3);
-        calc(8,30,4);
-        calc(8,35,4);
-        calc(8,40,4);
-        calc(8,30,5);
-        calc(8,35,5);
-        calc(8,40,5);
-        calc(8,30,6);
-        calc(8,35,6);
-        calc(8,40,6);
+        calc(8,3);
+        calc(9,3);
+        calc(10,3);
+        calc(11,3);
+        calc(8,4);
+        calc(8,5);
+        calc(8,6);
         }
-    public static void calc(int devs, int workweek, int weeks){
+    public static void calc(int devs, int weeks){
         double timereq = 1590;
+        int workweek = 30;
         double fin = (timereq/devs)/(workweek*weeks)*100;
+        System.out.printf("With %d devs working a %d week sprint, for %d hrs/wk, we would be at %.0f%% of capacity.%n",devs,weeks,workweek,fin);
+        workweek = 35;
+        fin = (timereq/devs)/(workweek*weeks)*100;
+        System.out.printf("With %d devs working a %d week sprint, for %d hrs/wk, we would be at %.0f%% of capacity.%n",devs,weeks,workweek,fin);
+        workweek = 40;
+        fin = (timereq/devs)/(workweek*weeks)*100;
         System.out.printf("With %d devs working a %d week sprint, for %d hrs/wk, we would be at %.0f%% of capacity.%n",devs,weeks,workweek,fin);
     }
 }
